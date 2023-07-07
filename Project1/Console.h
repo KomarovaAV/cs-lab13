1e4b7299aead50e3fc9d3bc56ff9a7395d6d7885
Ann <dusk1416@mail.ru>
2023-07-07 11:37:01 +0300

    добавление теста

---
+++ Project1/Console.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+// Функция поиска минимума и максимума среди чисел
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+
