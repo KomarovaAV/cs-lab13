a6bb48e07ecbcb08bbe02512eecef591e79e23b7
Ann <dusk1416@mail.ru>
2023-07-07 12:01:09 +0300

    тесты

--- Console.cpp
+++ Console.cpp
@@ -1,6 +1,8 @@
 #include "Console.h"
 using namespace std;
 void find_minmax(const vector<double>& numbers, double& min, double& max) {
+    if (numbers.size() == 0)
+        return;
     min = numbers[0];
     max = numbers[0];
     for (double x : numbers) {
--- Project1/test.cpp
+++ Project1/test.cpp
@@ -1,8 +1,8 @@
 #include "Console.h"
 #include <cassert>
 using namespace std;
-void
-test_positive() {
+
+void test_positive() {
     double min = 0;
     double max = 0;
     find_minmax({ 1, 2, 3 }, min, max);
@@ -10,7 +10,44 @@ test_positive() {
     assert(max == 3);
 }
 
-int
-main() {
-    test_positive();
+void test_negative() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ -1, -2, -3 }, min, max);
+    assert(min == -3);
+    assert(max == -1);
 }
+
+void test_same() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ 5, 5, 5 }, min, max);
+    assert(min == 5);
+    assert(max == 5);
+}
+
+void test_one() {
+    double min = 0;
+    double max = 0;
+    find_minmax({ 7 }, min, max);
+    assert(min == 7);
+    assert(max == 7);
+}
+
+void test_empty() {
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
+
+int main() {
+    test_positive();
+    test_negative();
+    test_same();
+    test_one();
+    test_empty();
+}
+
+
